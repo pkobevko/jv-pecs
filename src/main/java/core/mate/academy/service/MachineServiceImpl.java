701701c090901afeed4a57956972aab62a20a24f
Petro Kobevko <pkobevko@protonmail.com>
2025-07-29 09:18:44 +0300

    Added task solution

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,43 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Bulldozer;
+import core.mate.academy.model.Excavator;
+import core.mate.academy.model.Machine;
+import core.mate.academy.model.Truck;
+import core.mate.academy.model.Workable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl {
+public class MachineServiceImpl implements MachineService<Machine> {
+    private final BulldozerProducer bulldozerProducer = new BulldozerProducer();
+    private final ExcavatorProducer excavatorProducer = new ExcavatorProducer();
+    private final TruckProducer truckProducer = new TruckProducer();
+
+    @Override
+    public List<Machine> getAll(Class<? extends Machine> type) {
+        if (Bulldozer.class == type) {
+            return new ArrayList<>(bulldozerProducer.get());
+        }
+        if (Excavator.class == type) {
+            return new ArrayList<>(excavatorProducer.get());
+        }
+        if (Truck.class == type) {
+            return new ArrayList<>(truckProducer.get());
+        }
+        return List.of();
+    }
+
+    @Override
+    public void fill(List<? super Machine> machines, Machine value) {
+        Collections.fill(machines, value);
+    }
+
+    @Override
+    public void startWorking(List<? extends Machine> machines) {
+        machines.forEach(Workable::doWork);
+    }
 }
